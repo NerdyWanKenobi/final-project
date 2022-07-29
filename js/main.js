2639b99f46bb5fe27a8911665384ac8e96939742
Noah Nelson <nerdywankenobi@icloud.com>
2022-07-28 21:45:06 -0700

    Added Light Switch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  document.getElementsByTagName('button')[0].addEventListener('click', e => {
+    e.target.classList.toggle('on');
+  });
+}
+
+function rickRoll() {
+  alert("You have been Rickroll'd")
+}
+
+(function () {
+  document.getElementsByTagName('button')[0].addEventListener('click', e => {
+    e.target.classList.toggle('on');
+  });
+})();
