6a5adf2b55af6edfbc7f483e0b19dfac7c99b25b
NerdyWanKenobi <nerdywankenobi@icloud.com>
2022-08-04 18:08:26 -0700

    Added footer, updated dark mode

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
+/*
 function darkMode() {
-  /*var element = document.body;
+  /*
+  var element = document.body;
   element.classList.toggle("dark-mode");
   */
-  document.getElementsByTagName('button')[0].addEventListener('click', e => {
-    e.target.classList.toggle('on');
-  });
-  /*
-  var a = document.getElementsByClassName(bodyLink)
-  a.style.color = 'green';
-  */
+/*
+ document.getElementsByTagName('button')[0].addEventListener('click', e => {
+   e.target.classList.toggle('on');
+ });
+ /*
+ var a = document.getElementsByClassName(bodyLink)
+ a.style.color = 'green';
+ */
+/*
+ btn.addEventListener("click", function () {
+   // If the OS is set to dark mode...
+   if (prefersDarkScheme.matches) {
+     // ...then apply the .light-theme class to override those styles
+     document.body.classList.toggle("light-theme");
+     // Otherwise...
+   } else {
+     // ...apply the .dark-theme class to override the default light styles
+     document.body.classList.toggle("dark-theme");
+   }
+ });
+ */
+/*
 }
+*/
 
+/*
+document.getElementsByTagName('button')[0].addEventListener('click', e => {
+  e.target.classList.toggle('on');
+});
+*/
+
+/*
+(function() {
+  document.getElementsByTagName('button')[0].addEventListener('click', e => {
+  e.target.classList.toggle('on');
+});
+})();
+*/
+
+/*
 // Listen for a click on the button 
 btn.addEventListener("click", function () {
   // If the OS is set to dark mode...
@@ -24,64 +57,23 @@ btn.addEventListener("click", function () {
   }
 });
 
+
+function toggleDark() {
+// Listen for a click on the button 
+btn.addEventListener("click", function () {
+  // If the OS is set to dark mode...
+  if (prefersDarkScheme.matches) {
+    // ...then apply the .light-theme class to override those styles
+    document.body.classList.toggle("light-theme");
+    // Otherwise...
+  } else {
+    // ...apply the .dark-theme class to override the default light styles
+    document.body.classList.toggle("dark-theme");
+  }
+});
+}
+*/
+
 function rickRoll() {
   alert("You have been Rickroll'd")
 }
-
-/*
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora!
-
-// pneumonoultramicroscopicsilicovolcanoconiosis
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora!
-
-// pneumonoultramicroscopicsilicovolcanoconiosis
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-
-//    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae nihil quisquam saepe nesciunt itaque pariatur doloremque alias, totam magnam, molestiae eligendi corrupti ipsum non aut autem laudantium accusamus tempora! //
-
-// pneumonoultramicroscopicsilicovolcanoconiosis //
-*/
